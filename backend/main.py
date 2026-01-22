2ddfa06c6c111ad2d97684816c9fcf236dd78c34
oneusmt <oneusmtwo@gmail.com>
2026-01-22 23:15:32 +0300

    fixed

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,46 @@
-from typing import Union
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from src.config import settings
+from src.database import init_db
+from src.routes import offer_router, buy_router, sold_router
 
-app = FastAPI()
+
+app = FastAPI(
+    title=settings.app_name,
+    debug=settings.debug,
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= settings.cors_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.mount("/static", StaticFiles(directory=settings.static_dir), name="static")
+
+
+app.include_router(offer_router)
+app.include_router(buy_router)
+app.include_router(sold_router)
+
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def root():
+    return {"message": "Welcome to FastAPI TonAvto77!", "docs": "api/docs"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/health")
+def health_check():
+    return {"message": "Healthy!"}
