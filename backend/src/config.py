364153a753d78c4cf2436ba673f0864d02b6eee0
oneusmt <oneusmtwo@gmail.com>
2026-04-02 13:24:40 +0300

    add cors

--- backend/src/config.py
+++ backend/src/config.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
+        "http://111.88.125.130"
     ]
     static_dir: str = "static"
     images_dir: str = "static/images"
